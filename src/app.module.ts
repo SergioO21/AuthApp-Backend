e2f02a7aef8af98966d0aafa9bad90794521283d
Sergio Orejarena <sergioorueda@gmail.com>
2025-01-20 21:32:57 -0500

    env config

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,17 @@
+import { ConfigModule } from "@nestjs/config";
 import { Module } from "@nestjs/common";
-import { AppController } from "./app.controller";
-import { AppService } from "./app.service";
+import { MongooseModule } from "@nestjs/mongoose";
+
+import { AuthModule } from "./auth/auth.module";
+import * as process from "node:process";
 
 @Module({
-	imports: [],
-	controllers: [AppController],
-	providers: [AppService],
+	imports: [
+		ConfigModule.forRoot(),
+		MongooseModule.forRoot(<string>process.env.MONGO_URI),
+		AuthModule,
+	],
+	controllers: [],
+	providers: [],
 })
 export class AppModule {}
